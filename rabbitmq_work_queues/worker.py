d294534a4587ee622000b869ca18aee97eeebf07
tesfaye <tesfayegirma360@gmail.com>
2023-10-12 14:25:28 +0300

    worker queue or task queue

---
+++ rabbitmq_work_queues/worker.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import pika
+import time
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='task_queue', durable=True)
+print(' [*] Waiting for messages. To exit press CTRL+C')
+
+
+def callback(ch, method, properties, body):
+    """
+    A callback function that is called when a message is received.
+
+    Args:
+        ch (Channel): The channel object.
+        method (Method): The method object.
+        properties (Properties): The properties object.
+        body (bytes): The body of the message.
+
+    Returns:
+        None
+    """
+    print(f" [x] Received {body.decode()}")
+    time.sleep(body.count(b'.'))
+    print(" [x] Done")
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='task_queue', on_message_callback=callback)
+
+channel.start_consuming()
